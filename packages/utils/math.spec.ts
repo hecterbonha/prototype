1eb9bf9713ffd3548b73d3997845dd660c2f0da0
hecterbonha <hecterbonha@gmail.com>
2024-04-03 01:56:03 +0100

    chore(7006): integrated ci/cd pipeline with github actions

--- packages/utils/math.spec.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it } from "vitest";
-import { add } from "@/packages/utils/math";
-
-describe("math test", () => {
-  it("should add number", () => {
-    add(1, 2);
-  });
-});
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import '$lib/styles/main.css';
+</script>
+
+<h1 class="font-4xl">Hello</h1>
